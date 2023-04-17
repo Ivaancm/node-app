fed2eeb45353960322afd31f7a99f78ec1dbeae9
Ivan Carrasco <ivan.carrascomoreno@plexus.tech>
2023-04-17 15:34:30 +0200

    segundo commit, añadida la funcion añadir nueva nota controlada por post con verificadores de datos

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -2,8 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/notes/add', (req, res) => {
-    res.render()
+    res.render('notes/new-note');
 })
+
+router.post('/notes/new-note', (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+    if(!title) {
+        errors.push({text: 'por favor inserte o escriba un titulo'});
+    }
+    if(!description) {
+        errors.push({text: 'por favor inserte una descripcion'});
+    }
+    if(errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    } else {
+        res.send('ok');
+    }
+    
+});
 
 router.get('/notes', (req, res) => {
     res.send('notes desde la base de datos');
