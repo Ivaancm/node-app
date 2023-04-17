6ba644d94e55af81c3ac11495046cd0f4cb85f26
Ivan <ivan.carrascomoreno@plexus.tech>
2023-04-17 13:33:20 +0200

    commit inicial de nodejs con mongodb

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add', (req, res) => {
+    res.render()
+})
+
 router.get('/notes', (req, res) => {
     res.send('notes desde la base de datos');
 });
